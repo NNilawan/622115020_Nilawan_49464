55af188c2770fb5fbe1a319745c626ddb354f312
NNilawan <nilawan_l@cmu.ac.th>
2021-12-20 12:31:47 +0700

    Work 20-12-2021

---
+++ Flutter App Work/16-12-2021-secondApp/lib/pages/home.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:second_app/pages/detail.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text("Computer Knowledge"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          builder: (context, snapshot){
+            var data = json.decode(snapshot.data.toString());
+            return ListView.builder(itemBuilder: (BuildContext context, int index){
+              return MyBox(data[index]['title'], data[index]['subtitle'], data[index]['image']);
+            },
+            itemCount: data.length,
+            );
+          },
+          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),)
+      ),
+    );
+  }
+
+  Widget MyBox(String title, String subtitle, String imageLink){
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(16),
+      height: 160,
+      decoration: BoxDecoration(
+        color: Colors.blue[200],
+        borderRadius: BorderRadius.circular(16),
+        image: DecorationImage(
+          image: NetworkImage(
+            imageLink
+          ),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken)
+        )
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            title, 
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(
+            height: 8,),
+          Text(
+            subtitle, 
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white
+            ),
+          ),
+          SizedBox(
+            height: 7,
+          ),
+          TextButton(
+            onPressed: (){
+              print("Next Pages >>");
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context)=>DetailPage()));
+            }, 
+            child: Text("read more")
+            )
+        ]
+      ),
+    );
+  }
+
+}
+
