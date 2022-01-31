02db6dc9b383d3efe8d12cee8830689b3fbd70b3
NNilawan <86776453+NNilawan@users.noreply.github.com>
2022-01-31 12:42:28 +0700

    Add files via upload

---
+++ Flutter App Work/27-31-01-2022/lib/sign_in_page.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  // const SignInPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Authentication'),
+      ),
+      body: _buildContent(),
+    );
+  }
+}
+
+Future<void> _signInAnonymously() async {
+  try{
+    final userCredentials = await FirebaseAuth.instance.signInAnonymously();
+    print('${userCredentials.user!.uid}');
+  } catch (e) {
+    print(e.toString());
+  }
+}
+
+Widget _buildContent() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        Text(
+          "Sign In",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w600          
+            ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        ElevatedButton(
+          onPressed: () => _signInAnonymously(), 
+          child: Text("Go Anonymouse")
+        ),
+      ],
+    ),
+  );
+}
